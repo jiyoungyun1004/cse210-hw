2a73879a16d6dd83841c77ba23abe50506c587e2
jiyoungyun1004 <yun22003@byui.edu>
2025-12-04 11:33:21 -0700

    Complete Learning02 assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!!!");
+        // First job entry
+        var job1 = new Job
+        {
+            _jobTitle = "Math Tutor",
+            _company = "Varsity Tutors",
+            _startYear = 2023,
+            _endYear = 2023
+        };
+
+        // Second job entry
+        var job2 = new Job
+        {
+            _jobTitle = "AI Trainer",
+            _company = "Handshake",
+            _startYear = 2025,
+            _endYear = 2026
+        };
+
+        // Build resume
+        var myResume = new Resume
+        {
+            _name = "Sophia Yun"
+        };
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display final resume
+        myResume.Display();
     }
 }
