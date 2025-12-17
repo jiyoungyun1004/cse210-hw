f37a4b1246c0bf8a220172e2ea772c778d75c585
jiyoungyun1004 <yun22003@byui.edu>
2025-12-17 13:13:46 -0700

    w 07 prep learning activity

---
+++ prepare/Learning04/writingassignment.cs
@@ -0,0 +1,15 @@
+public class WritingAssignment : Assignment
+{
+    private string title;
+
+    public WritingAssignment(string studentName, string topic, string title)
+        : base(studentName, topic)
+    {
+        this.title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{title} by {GetStudentName()}";
+    }
+}
